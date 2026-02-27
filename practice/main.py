f6cf9bd935d7f4d49aa62a33b833ec7f4172e201
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-02-27 15:38:40 +0530

    work with basemodel design

--- practice/main.py
+++ practice/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -14,3 +15,15 @@ def name(name:str):
 @app.get('/search')
 def products(name:str, price:int):
     return {"product":name,"productprice":price}
+
+class products(BaseModel):
+    name:str
+    price:int
+
+@app.post('/product')
+def list(product:products):
+    return{
+        "Item name":product.name,
+        "pricetag":product.price
+
+    }
