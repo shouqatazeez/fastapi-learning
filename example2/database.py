e119a8ec84fa6a66faedd8391e48843833971215
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-02-27 10:30:21 +0530

    created the database connection

--- example2/database.py
+++ example2/database.py
@@ -1,5 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy import declartive_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import DeclarativeBase
+
 
 engine = create_engine("sqlite:///database.db", connect_args = {"check_same_thread": False}
 )
+
+sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+class Base(DeclarativeBase):
+    pass
+
