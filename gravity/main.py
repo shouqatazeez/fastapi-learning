4ebdc7d1f292957a18877ae2c120dc0272625059
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-03-01 14:59:32 +0530

    work with path parmeters

--- gravity/main.py
+++ gravity/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get('/status')
 def server_status():
     return {"status":"running smoothly"}
+
+@app.get('/users/{username}')
+def username(username):
+    return  {"greeting": f"Hello, {username}!"}
