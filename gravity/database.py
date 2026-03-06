7ce4283d2613a693d05ae1c4f315d308ae5057e5
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-03-06 15:19:21 +0530

    added the Base i.e declarative base for the blue print

--- gravity/database.py
+++ gravity/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
+from sqlalchemy import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thred":False})
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
