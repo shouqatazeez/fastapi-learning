0670519489ea03568ce134a672c46373bf9beee5
Mohammad Shouqat Azeez <mdshouqatazeez@gmail.com>
2026-03-06 15:22:03 +0530

    added some minor changes in database file

--- gravity/database.py
+++ gravity/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thred":False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
